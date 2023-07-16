0fec6b7e78bbbac5c50b49ba8016f93e0dcc5df6
EduWoldhuis <98768570+EduWoldhuis@users.noreply.github.com>
2023-07-16 23:24:08 +0200

    Initial commit

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    },
+    ["<leader>+"] = {
+      "<cmd> !g++ % --debug -o %:r<CR>",
+      "Compile current file with debug symbols",
+    },
+  }
+
+}
+
+return M
